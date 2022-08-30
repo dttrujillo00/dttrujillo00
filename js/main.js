1dcccd7953aa10b36f44e6d6394cf4902c92f99c
dttrujillo00 <dttrujillo00@gmail.com>
2022-08-30 15:00:13 -0400

    First commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const bannerContent = document.querySelector('.banner_content');
+    const bannerBtn = document.querySelector('.banner .btn');
+    
+    bannerContent.classList.add('show');
+    bannerBtn.classList.add('flash');
+
+})
